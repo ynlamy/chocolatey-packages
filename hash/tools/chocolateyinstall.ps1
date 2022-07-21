2cd1635cb5c9f97ad7ed0701b85c151410a1b46a
Yoann LAMY <ynlamy@gmail.com>
2022-07-21 20:25:10 +0200

    Add files via upload

---
+++ hash/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = "$toolsDir\hash_setup.exe"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  file           = $fileLocation
+  softwareName   = 'hash*'
+  silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
+  validExitCodes = @(0)
+}
+
+$pp = Get-PackageParameters
+if ($pp.UseInf) {
+  if (Test-Path "$($pp.UseInf)") {
+    Write-Host "Using existing configuration file at '$($pp.UseInf)'"
+    $packageArgs['silentArgs'] = "$($packageArgs['silentArgs']) /LOADINF=`"$($pp.UseInf)`""
+  } else {
+    Write-Host "Creating new configuration file at '$($pp.UseInf)'"
+    $packageArgs['silentArgs'] = "$($packageArgs['silentArgs']) /SAVEINF=`"$($pp.UseInf)`""
+  }
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
+
+Remove-Item $fileLocation -ea 0 -Force
---
+++ hash/tools/chocolateyuninstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'hash*'
+  fileType      = 'exe'
+  silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoUnInstall.log`""
+  validExitCodes= @(0)
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $packageArgs['file'] = "$($key[0].UninstallString)"
+  Uninstall-ChocolateyPackage @packageArgs
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
+}
